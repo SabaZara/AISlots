dbd43af8a89a67f98947ed15c5661abb39e3a243
SabaZara <sabazarandia007@gmail.com>
2026-07-18 18:00:15 +0400

    Lift mobile game composition

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ import {
   THEMES,
   resolveVisualConfig,
   visualConfigLabel
-} from "./asset-catalog.js?v=4.8.0";
+} from "./asset-catalog.js?v=4.8.1";
 
 const BET_OPTIONS = [1, 2, 5, 10, 20];
 const MIN_RESULT_DISPLAY_MS = 2500;
